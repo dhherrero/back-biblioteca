67d2971fa3bf73e07ca87d779f02c011052e3b6e
Diego Herrero <d.herrero8@usp.ceu.es>
2023-02-09 10:43:43 +0100

    Prueba funcionando

--- src/main/java/com/app/backbiblioteca/Back/Biblioteca/BackBibliotecaApplication.java
+++ src/main/java/com/app/backbiblioteca/Back/Biblioteca/BackBibliotecaApplication.java
@@ -2,8 +2,10 @@ package com.app.backbiblioteca.Back.Biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.app.backbiblioteca.Back.controller"})
 public class BackBibliotecaApplication {
 
 	public static void main(String[] args) {
