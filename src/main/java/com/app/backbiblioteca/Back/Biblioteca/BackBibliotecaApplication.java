106a7d8780703d798ca5fb21171c64c15547376f
Diego Herrero <d.herrero8@usp.ceu.es>
2023-02-09 10:39:30 +0100

    First commit

---
+++ src/main/java/com/app/backbiblioteca/Back/Biblioteca/BackBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.app.backbiblioteca.Back.Biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBibliotecaApplication.class, args);
+	}
+
+}
